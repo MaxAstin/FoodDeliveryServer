7e80952b0e78c861c9f41af1bfde9108bb30df19
maxastin <max.astin@yandex.ru>
2021-12-17 18:49:28 +0300

    update:order changing

--- src/main/kotlin/com/bunbeauty/fooddelivery/service/order/OrderService.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/service/order/OrderService.kt
@@ -62,9 +62,10 @@ class OrderService(private val orderRepository: IOrderRepository, private val st
 
         val clientOrder = orderRepository.getOrderByUuid(orderUuid.toUuid())
         println("changeOrder clientOrder = ${clientOrder?.uuid}")
-        if (clientOrder != null) {
-            println("changeOrder listener = ${listenerMap[orderUuid]}")
-            listenerMap[orderUuid]?.emit(clientOrder)
+        val clientUserUuid = changedOrder?.clientUser?.uuid
+        if (clientOrder != null && clientUserUuid != null) {
+            println("changeOrder listener = ${listenerMap[clientUserUuid]}")
+            listenerMap[clientUserUuid]?.emit(clientOrder)
         }
 
         return changedOrder
