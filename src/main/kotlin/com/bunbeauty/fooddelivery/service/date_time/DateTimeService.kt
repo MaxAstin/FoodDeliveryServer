da5a5b277ee0645a29a103d4d8aa12dbd7a1e6cb
maxastin <max.astin@yandex.ru>
2022-01-08 17:51:34 +0300

    update:statistic dates

--- src/main/kotlin/com/bunbeauty/fooddelivery/service/date_time/DateTimeService.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/service/date_time/DateTimeService.kt
@@ -1,7 +1,5 @@
 package com.bunbeauty.fooddelivery.service.date_time
 
-import com.bunbeauty.fooddelivery.data.Constants.DD_MMMM_YYYY_PATTERN
-import com.bunbeauty.fooddelivery.data.Constants.MMMM_YYYY_PATTERN
 import org.joda.time.DateTime
 import org.joda.time.DateTimeConstants.MONDAY
 import org.joda.time.DateTimeConstants.SUNDAY
@@ -11,7 +9,7 @@ class DateTimeService : IDateTimeService {
     override fun getDateDDMMMMYYYY(millis: Long): String {
         val dateTime = DateTime(millis)
 
-        return dateTime.toString(DD_MMMM_YYYY_PATTERN)
+        return dateTime.dayOfMonth.toString() + " " + getMonthNameInclined(dateTime.monthOfYear) + " " + dateTime.year
     }
 
     override fun getWeekPeriod(millis: Long): String {
@@ -24,7 +22,40 @@ class DateTimeService : IDateTimeService {
 
     override fun getDateMMMMYYYY(millis: Long): String {
         val dateTime = DateTime(millis)
+        return getMonthName(dateTime.monthOfYear) + " " + dateTime.year
+    }
 
-        return dateTime.toString(MMMM_YYYY_PATTERN)
+    fun getMonthName(monthNumber: Int): String {
+        return when (monthNumber) {
+            1 -> "январь"
+            2 -> "февраль"
+            3 -> "март"
+            4 -> "апрель"
+            5 -> "май"
+            6 -> "июнь"
+            7 -> "июль"
+            8 -> "август"
+            9 -> "сентябрь"
+            10 -> "октябрь"
+            11 -> "ноябрь"
+            else -> "декабрь"
+        }
+    }
+
+    fun getMonthNameInclined(monthNumber: Int): String {
+        return when (monthNumber) {
+            1 -> "января"
+            2 -> "февраля"
+            3 -> "марта"
+            4 -> "апреля"
+            5 -> "мая"
+            6 -> "июня"
+            7 -> "июля"
+            8 -> "августа"
+            9 -> "сентября"
+            10 -> "октября"
+            11 -> "ноября"
+            else -> "декабря"
+        }
     }
 }
