8015067effb172002e291ad0f4b5451f0f4e3d0a
maxastin <max.astin@yandex.ru>
2022-02-14 00:31:35 +0300

    fix:client user login session creation

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/model/client_user/login/GetClientUserLoginSession.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/model/client_user/login/GetClientUserLoginSession.kt
@@ -1,5 +1,8 @@
 package com.bunbeauty.fooddelivery.data.model.client_user.login
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GetClientUserLoginSession(
     val uuid: String,
     val phoneNumber: String,
--- src/main/kotlin/com/bunbeauty/fooddelivery/data/model/client_user/login/GetClientUserLoginSessionUuid.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/model/client_user/login/GetClientUserLoginSessionUuid.kt
@@ -1,5 +1,8 @@
 package com.bunbeauty.fooddelivery.data.model.client_user.login
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GetClientUserLoginSessionUuid(
     val uuid: String
 )
