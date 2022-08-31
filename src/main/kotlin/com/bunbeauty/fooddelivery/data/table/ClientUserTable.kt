4630db38c85062e572297fc514063ab733e41902
maxastin <max.astin@yandex.ru>
2022-08-31 23:36:01 +0300

    update:client user table

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/table/ClientUserTable.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/table/ClientUserTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 
 object ClientUserTable : UUIDTable() {
 
-    val phoneNumber = varchar("phoneNumber", 512).uniqueIndex()
+    val phoneNumber = varchar("phoneNumber", 512)
     val email = varchar("email", 512).nullable()
     val company = reference("company", CompanyTable)
 }
