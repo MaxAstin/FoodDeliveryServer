aa9b26bc8cd69d652e68f159c87812ba905817ee
maxastin <max.astin@yandex.ru>
2022-09-16 22:45:27 +0300

    refactor:date formatting

--- src/main/kotlin/com/bunbeauty/fooddelivery/service/date_time/DateTimeService.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/service/date_time/DateTimeService.kt
@@ -4,12 +4,46 @@ import org.joda.time.DateTime
 import org.joda.time.DateTimeConstants.MONDAY
 import org.joda.time.DateTimeConstants.SUNDAY
 
+private const val DEFAULT_MONTH = "декабрь"
+private val MONTH_MAP = mapOf(
+    1 to "январь",
+    2 to "февраль",
+    3 to "март",
+    4 to "апрель",
+    5 to "май",
+    6 to "июнь",
+    7 to "июль",
+    8 to "август",
+    9 to "сентябрь",
+    10 to "октябрь",
+    11 to "ноябрь",
+    12 to "декабрь",
+)
+
+private const val DEFAULT_MONTH_IN_GENITIVE = "декабря"
+private val MONTH_IN_GENITIVE_MAP = mapOf(
+    1 to "января",
+    2 to "февраля",
+    3 to "марта",
+    4 to "апреля",
+    5 to "мая",
+    6 to "июня",
+    7 to "июля",
+    8 to "августа",
+    9 to "сентября",
+    10 to "октября",
+    11 to "ноября",
+    12 to "декабря",
+)
+
 class DateTimeService : IDateTimeService {
 
     override fun getDateDDMMMMYYYY(millis: Long): String {
         val dateTime = DateTime(millis)
 
-        return dateTime.dayOfMonth.toString() + " " + getMonthNameInclined(dateTime.monthOfYear) + " " + dateTime.year
+        return dateTime.dayOfMonth.toString() + " " +
+                MONTH_IN_GENITIVE_MAP.getOrDefault(dateTime.monthOfYear, DEFAULT_MONTH_IN_GENITIVE) + " " +
+                dateTime.year
     }
 
     override fun getWeekPeriod(millis: Long): String {
@@ -22,40 +56,6 @@ class DateTimeService : IDateTimeService {
 
     override fun getDateMMMMYYYY(millis: Long): String {
         val dateTime = DateTime(millis)
-        return getMonthName(dateTime.monthOfYear) + " " + dateTime.year
-    }
-
-    fun getMonthName(monthNumber: Int): String {
-        return when (monthNumber) {
-            1 -> "январь"
-            2 -> "февраль"
-            3 -> "март"
-            4 -> "апрель"
-            5 -> "май"
-            6 -> "июнь"
-            7 -> "июль"
-            8 -> "август"
-            9 -> "сентябрь"
-            10 -> "октябрь"
-            11 -> "ноябрь"
-            else -> "декабрь"
-        }
-    }
-
-    fun getMonthNameInclined(monthNumber: Int): String {
-        return when (monthNumber) {
-            1 -> "января"
-            2 -> "февраля"
-            3 -> "марта"
-            4 -> "апреля"
-            5 -> "мая"
-            6 -> "июня"
-            7 -> "июля"
-            8 -> "августа"
-            9 -> "сентября"
-            10 -> "октября"
-            11 -> "ноября"
-            else -> "декабря"
-        }
+        return MONTH_MAP.getOrDefault(dateTime.monthOfYear, DEFAULT_MONTH) + " " + dateTime.year
     }
 }
