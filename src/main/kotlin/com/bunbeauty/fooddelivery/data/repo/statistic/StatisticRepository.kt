c09d819489d7b5ad45a1039bd3edd712863d02b0
v.podkopaev <max.astin@yandex.ru>
2023-02-16 23:42:14 +0200

    fix:new day statistic

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/repo/statistic/StatisticRepository.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/repo/statistic/StatisticRepository.kt
@@ -4,12 +4,13 @@ import com.bunbeauty.fooddelivery.data.DatabaseFactory.query
 import com.bunbeauty.fooddelivery.data.entity.CompanyEntity
 import com.bunbeauty.fooddelivery.data.entity.StatisticDayEntity
 import com.bunbeauty.fooddelivery.data.entity.StatisticDayProductEntity
+import com.bunbeauty.fooddelivery.data.ext.toUuid
 import com.bunbeauty.fooddelivery.data.model.statistic.insert.InsertStatisticDay
 
 class StatisticRepository : IStatisticRepository {
 
     override suspend fun insetStatisticDay(insertStatisticDay: InsertStatisticDay) = query {
-        val statisticDay = StatisticDayEntity.new {
+        val insertedStatisticDay = StatisticDayEntity.new {
             time = insertStatisticDay.time
             orderCount = insertStatisticDay.orderCount
             orderProceeds = insertStatisticDay.proceeds
@@ -21,8 +22,9 @@ class StatisticRepository : IStatisticRepository {
                 photoLink = insertStatisticProduct.photoLink
                 productCount = insertStatisticProduct.productCount
                 productProceeds = insertStatisticProduct.proceeds
+                statisticDay = insertedStatisticDay
             }
         }
-        statisticDay.toStatisticDay()
+        insertedStatisticDay.toStatisticDay()
     }
 }
