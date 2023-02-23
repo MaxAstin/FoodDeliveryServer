58ce408c9b6e28b5c61c7c118ad7761941928121
v.podkopaev <max.astin@yandex.ru>
2023-02-23 22:04:30 +0200

    update: getting version

--- src/main/kotlin/com/bunbeauty/fooddelivery/routing/VersionRouting.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/routing/VersionRouting.kt
@@ -1,8 +1,7 @@
 package com.bunbeauty.fooddelivery.routing
 
 import com.bunbeauty.fooddelivery.data.Constants.COMPANY_UUID_PARAMETER
-import com.bunbeauty.fooddelivery.routing.extension.respondBad
-import com.bunbeauty.fooddelivery.routing.extension.respondOk
+import com.bunbeauty.fooddelivery.routing.extension.respondOkOrBad
 import com.bunbeauty.fooddelivery.routing.extension.safely
 import com.bunbeauty.fooddelivery.service.version.IVersionService
 import io.ktor.server.application.*
@@ -24,11 +23,10 @@ fun Routing.getForceUpdateVersion() {
         safely {
             val companyUuid = call.parameters[COMPANY_UUID_PARAMETER] ?: error("$COMPANY_UUID_PARAMETER is required")
             val forceUpdateVersion = versionService.getForceUpdateVersionByCompanyUuid(companyUuid)
-            if (forceUpdateVersion == null) {
-                call.respondBad("No forceUpdateVersion with companyUuid = $companyUuid")
-            } else {
-                call.respondOk(forceUpdateVersion)
-            }
+            call.respondOkOrBad(
+                model = forceUpdateVersion,
+                errorMessage = "No forceUpdateVersion with companyUuid = $companyUuid"
+            )
         }
     }
 }
