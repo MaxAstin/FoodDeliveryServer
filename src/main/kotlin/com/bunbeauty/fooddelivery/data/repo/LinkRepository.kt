220375b3373e84df43a171463f69ae9e40ffcf1e
v.podkopaev <max.astin@yandex.ru>
2023-05-15 22:47:31 +0300

    fix: getting links

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/repo/LinkRepository.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/repo/LinkRepository.kt
@@ -3,14 +3,14 @@ package com.bunbeauty.fooddelivery.data.repo
 import com.bunbeauty.fooddelivery.data.DatabaseFactory.query
 import com.bunbeauty.fooddelivery.data.entity.company.LinkEntity
 import com.bunbeauty.fooddelivery.data.model.company.link.GetLink
-import com.bunbeauty.fooddelivery.data.table.PaymentMethodTable
-import java.util.UUID
+import com.bunbeauty.fooddelivery.data.table.LinkTable
+import java.util.*
 
 class LinkRepository {
 
     suspend fun getLinkListByCompanyUuid(companyUuid: UUID): List<GetLink> = query {
         LinkEntity.find {
-            PaymentMethodTable.company eq companyUuid
+            LinkTable.company eq companyUuid
         }.map { linkEntity ->
             linkEntity.toLink()
         }
