d0ed1b7094b49da3ce7204085de49bb55c09972f
v.podkopaev <max.astin@yandex.ru>
2023-09-07 00:24:16 +0300

    add client ip mock

--- src/main/kotlin/com/bunbeauty/fooddelivery/routing/AuthorizationRouting.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/routing/AuthorizationRouting.kt
@@ -29,7 +29,7 @@ private fun Routing.sendCode() {
             authorizationService.sendCode(
                 companyUuid = companyUuid,
                 postClientCodeRequest = postClientCodeRequest,
-                clientIp = clientIp
+                clientIp = "1.1.1.1"// TODO get back clientIp
             )
         }
     }
