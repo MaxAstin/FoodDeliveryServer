52adb4e337d91cdd09eb45d8e55822b402b2bac8
v.podkopaev <max.astin@yandex.ru>
2023-09-07 23:06:14 +0300

    add logs for networkModule

--- src/main/kotlin/com/bunbeauty/fooddelivery/di/NetworkModule.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/di/NetworkModule.kt
@@ -14,8 +14,8 @@ import org.koin.dsl.module
 private const val AUTH_API_EMAIL_KEY = "AUTH_API_EMAIL_KEY"
 private const val AUTH_API_KEY = "AUTH_API_KEY"
 
-val authApiEmail = System.getenv(AUTH_API_EMAIL_KEY)
-val authApiKey = System.getenv(AUTH_API_KEY)
+private val authApiEmail = System.getenv(AUTH_API_EMAIL_KEY)
+private val authApiKey = System.getenv(AUTH_API_KEY)
 
 val networkModule = module(createdAtStart = true) {
     factory {  NetworkService(get()) }
@@ -33,6 +33,12 @@ val networkModule = module(createdAtStart = true) {
             }
 
             install(DefaultRequest) {
+                println("\n")
+                println("======================================")
+                println("authApiEmail $authApiEmail")
+                println("authApiKey $authApiKey")
+                println("======================================")
+                println("\n")
                 host = "$authApiEmail:$authApiKey@gate.smsaero.ru/v2"
                 header(HttpHeaders.ContentType, ContentType.Application.Json)
 
