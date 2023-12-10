b3a760fdf7aafaa443cf1d7aa47ba4f3324022d5
v.podkopaev <max.astin@yandex.ru>
2023-12-10 23:10:59 +0200

    add suggestion logs

--- src/main/kotlin/com/bunbeauty/fooddelivery/network/NetworkExtensions.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/network/NetworkExtensions.kt
@@ -51,5 +51,8 @@ suspend inline fun <reified R> HttpClient.postData(
 }
 
 fun <T> ApiResult<T>.getDataOrNull(): T? {
+    if (this is ApiResult.Error) {
+        println(throwable.message)
+    }
     return (this as? ApiResult.Success)?.data
 }
