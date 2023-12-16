c5c6817dc0a7b782a83f79070fcec1adbc5742dc
v.podkopaev <max.astin@yandex.ru>
2023-12-16 13:11:18 +0200

    add logs for suggestions request

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/features/address/AddressNetworkDataSource.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/features/address/AddressNetworkDataSource.kt
@@ -6,23 +6,22 @@ import com.bunbeauty.fooddelivery.network.ApiResult
 import com.bunbeauty.fooddelivery.network.safeCall
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 
 class AddressNetworkDataSource(private val client: HttpClient) {
 
     suspend fun requestAddressSuggestions(addressRequestBody: AddressRequestBody): ApiResult<SuggestionsResponse> {
-//        return client.postData(
-//            path = "address",
-//            body = addressRequestBody
-//        )
-
         return safeCall {
-            client.post {
+            val response = client.post {
                 setBody(addressRequestBody)
                 url {
                     path("address")
                 }
             }
+            println(response.bodyAsText())
+
+            response
         }
     }
 
