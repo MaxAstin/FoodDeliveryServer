70883381ebe03fbda5842ae970fe167119dea3e4
v.podkopaev <max.astin@yandex.ru>
2023-12-25 21:26:19 +0200

    add default values for AddressV2Table

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/table/AddressTableV2.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/table/AddressTableV2.kt
@@ -6,8 +6,8 @@ object AddressV2Table : UUIDTable() {
 
     val streetFiasId = varchar("streetFiasId", 512)
     val streetName = varchar("streetName", 512)
-    val streetLatitude = double("streetLatitude")
-    val streetLongitude = double("streetLongitude")
+    val streetLatitude = double("streetLatitude").default(0.0)
+    val streetLongitude = double("streetLongitude").default(0.0)
     val house = varchar("house", 512)
     val flat = varchar("flat", 512).nullable()
     val entrance = varchar("entrance", 512).nullable()
