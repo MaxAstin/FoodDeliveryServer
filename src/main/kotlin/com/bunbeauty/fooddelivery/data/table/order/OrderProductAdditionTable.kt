cc589729cf7fd7ce6d79534880410695c98b50af
v.podkopaev <max.astin@yandex.ru>
2023-12-26 11:29:12 +0200

    fix OrderProductAddition table

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/table/order/OrderProductAdditionTable.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/table/order/OrderProductAdditionTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 
 object OrderProductAdditionTable : UUIDTable() {
 
-    val name = OrderProductTable.varchar("name", 512)
+    val name = varchar("name", 512)
     val price = integer("price").nullable()
     val orderProduct = reference("orderProduct", OrderProductTable)
 
