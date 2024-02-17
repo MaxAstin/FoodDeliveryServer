3a5572f7cee9a1430e4b24d2cab52763bbaa3146
v.podkopaev <max.astin@yandex.ru>
2024-02-17 11:37:18 +0200

    remove old recommendations

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/repo/order/OrderStatisticRepository.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/repo/order/OrderStatisticRepository.kt
@@ -2,6 +2,7 @@ package com.bunbeauty.fooddelivery.data.repo.order
 
 import com.bunbeauty.fooddelivery.data.DatabaseFactory.query
 import com.bunbeauty.fooddelivery.data.entity.order.OrderEntity
+import com.bunbeauty.fooddelivery.data.enums.OrderStatus
 import com.bunbeauty.fooddelivery.data.table.order.OrderTable
 import com.bunbeauty.fooddelivery.domain.model.statistic.GetStatisticOrder
 import org.jetbrains.exposed.sql.and
@@ -15,7 +16,15 @@ class OrderStatisticRepository : IOrderStatisticRepository {
                 (OrderTable.cafe eq cafeUuid) and
                         (OrderTable.time greater fromTime) and
                         (OrderTable.time less toTime) and
-                        (OrderTable.status eq "DELIVERED")
+                        OrderTable.status.inList(
+                            listOf(
+                                OrderStatus.ACCEPTED.name,
+                                OrderStatus.PREPARING.name,
+                                OrderStatus.SENT_OUT.name,
+                                OrderStatus.DELIVERED.name,
+                                OrderStatus.DONE.name,
+                            )
+                        )
             }.map { orderEntity ->
                 orderEntity.toStatisticOrder()
             }
@@ -30,7 +39,15 @@ class OrderStatisticRepository : IOrderStatisticRepository {
             (OrderTable.company eq companyUuid) and
                     (OrderTable.time greater fromTime) and
                     (OrderTable.time less toTime) and
-                    (OrderTable.status eq "DELIVERED")
+                    OrderTable.status.inList(
+                        listOf(
+                            OrderStatus.ACCEPTED.name,
+                            OrderStatus.PREPARING.name,
+                            OrderStatus.SENT_OUT.name,
+                            OrderStatus.DELIVERED.name,
+                            OrderStatus.DONE.name,
+                        )
+                    )
         }.map { orderEntity ->
             orderEntity.toStatisticOrder()
         }
