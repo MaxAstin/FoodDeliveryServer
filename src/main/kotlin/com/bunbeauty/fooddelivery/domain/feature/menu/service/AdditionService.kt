dffa3730f3a4c38f46b07bdfedcb3af07cd8c130
v.podkopaev <max.astin@yandex.ru>
2024-03-13 00:00:40 +0300

    fix postAdditionToGroup flow

--- src/main/kotlin/com/bunbeauty/fooddelivery/domain/feature/menu/service/AdditionService.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/domain/feature/menu/service/AdditionService.kt
@@ -73,7 +73,7 @@ class AdditionService(
             companyUuid = companyUuid
         )
         checkAdditionGroupAvailability(
-            additionGroupUuid = postAdditionToGroup.additionUuid,
+            additionGroupUuid = postAdditionToGroup.groupUuid,
             companyUuid = companyUuid
         )
 
