e066de28d8a9cfdc174646a0cb884190d75da6a3
v.podkopaev <max.astin@yandex.ru>
2024-03-12 23:50:43 +0300

    fix addition group mapping

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/features/menu/mapper/AdditionGroupEntityMapper.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/features/menu/mapper/AdditionGroupEntityMapper.kt
@@ -4,11 +4,12 @@ import com.bunbeauty.fooddelivery.data.entity.menu.AdditionGroupEntity
 import com.bunbeauty.fooddelivery.domain.feature.menu.model.addition.AdditionGroup
 
 val mapToAdditionGroup: AdditionGroupEntity.() -> AdditionGroup = {
-   AdditionGroup(
+    AdditionGroup(
         uuid = uuid,
         name = name,
         singleChoice = singleChoice,
         priority = priority,
         isVisible = isVisible,
+        companyUuid = company.uuid,
     )
 }
