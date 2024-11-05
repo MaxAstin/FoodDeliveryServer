487a84b7f262b8682689b7d2747d175241ac4f5a
v.podkopaev <max.astin@yandex.ru>
2024-11-06 00:42:09 +0200

    add analytics label for sending notifications

--- src/main/kotlin/com/bunbeauty/fooddelivery/service/NotificationService.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/service/NotificationService.kt
@@ -4,6 +4,7 @@ import com.bunbeauty.fooddelivery.data.features.user.UserRepository
 import com.bunbeauty.fooddelivery.domain.error.orThrowNotFoundByUuidError
 import com.bunbeauty.fooddelivery.domain.model.notification.PostNotification
 import com.bunbeauty.fooddelivery.domain.toUuid
+import com.google.firebase.messaging.FcmOptions
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
@@ -44,6 +45,9 @@ class NotificationService(
                 )
                 .putData(ORDER_CODE_KEY, orderCode)
                 .setTopic(cafeUuid)
+                .setFcmOptions(
+                    FcmOptions.withAnalyticsLabel("order")
+                )
                 .build()
         )
     }
