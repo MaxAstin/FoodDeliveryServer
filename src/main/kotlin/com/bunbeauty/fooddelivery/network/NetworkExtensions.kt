16c0d7397c20909fc51aa57fd92ba21ca046d7e2
xIdealo <shavl.mark@yandex.ru>
2025-01-02 00:58:37 +0300

    added tests and check ktlint

--- src/main/kotlin/com/bunbeauty/fooddelivery/network/NetworkExtensions.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/network/NetworkExtensions.kt
@@ -1,7 +1,12 @@
 package com.bunbeauty.fooddelivery.network
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.path
 
 suspend inline fun <reified R> HttpClient.getData(
     path: String,
@@ -19,7 +24,7 @@ suspend inline fun <reified R> HttpClient.getData(
 
 suspend inline fun <reified R> HttpClient.postData(
     path: String,
-    body: Any,
+    body: Any  ,
     parameters: Map<String, Any> = mapOf()
 ): ApiResult<R> {
     return safeCall {
