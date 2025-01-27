e1336e94d2bbc538e937421528f9139fc485f493
Mark Shavlovskij <36772847+xidealo@users.noreply.github.com>
2025-01-28 01:09:49 +0300

    feature/update gwif (#8)
    * fix PatchCompany
    
    * fix PatchCompany

--- src/main/kotlin/com/bunbeauty/fooddelivery/domain/feature/company/usecase/GetWorkInfoUseCase.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/domain/feature/company/usecase/GetWorkInfoUseCase.kt
@@ -3,18 +3,26 @@ package com.bunbeauty.fooddelivery.domain.feature.company.usecase
 import com.bunbeauty.fooddelivery.data.repo.CompanyRepository
 import com.bunbeauty.fooddelivery.domain.error.orThrowNotFoundByUuidError
 import com.bunbeauty.fooddelivery.domain.model.company.work_info.WorkInfo
+import com.bunbeauty.fooddelivery.domain.model.company.work_info.WorkType
 import com.bunbeauty.fooddelivery.domain.toUuid
 
 class GetWorkInfoUseCase(
     private val companyRepository: CompanyRepository
 ) {
-
+    /**
+     * Для обартной совместимости есть проверка на isOpen
+     * В версии 3.0 удалить
+     * */
     suspend operator fun invoke(companyUuid: String): WorkInfo {
         val company = companyRepository.getCompanyByUuid(uuid = companyUuid.toUuid())
             .orThrowNotFoundByUuidError(uuid = companyUuid)
 
         return WorkInfo(
-            workType = company.workType
+            workType = if (company.isOpen) {
+                company.workType
+            } else {
+                WorkType.CLOSED
+            }
         )
     }
 }
