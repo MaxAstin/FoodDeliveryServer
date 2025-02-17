eb5544d4adc81a36e6dea30cabb4579dcbeb806f
xIdealo <shavl.mark@yandex.ru>
2025-02-17 23:15:19 +0300

    try add new field for user

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/table/UserTable.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/table/UserTable.kt
@@ -1,8 +1,12 @@
 package com.bunbeauty.fooddelivery.data.table
 
 import com.bunbeauty.fooddelivery.data.enums.UserRole
+import com.bunbeauty.fooddelivery.data.table.cafe.CafeTable
 import org.jetbrains.exposed.dao.id.UUIDTable
 
+/**
+ * Table for admin app
+ * */
 object UserTable : UUIDTable() {
 
     val username = varchar("username", 512).uniqueIndex()
@@ -12,5 +16,9 @@ object UserTable : UUIDTable() {
     val updateNotificationTokenDateTime = varchar("updateNotificationTokenDateTime", 512).nullable()
     val unlimitedNotification = bool("unlimitedNotification").default(true)
     val role = enumeration("role", UserRole::class)
+
+    @Deprecated("Use cafe to connect tables")
     val city = reference("city", CityTable)
+
+    val cafe = reference("cafe", CafeTable)
 }
