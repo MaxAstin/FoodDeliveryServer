05b53dd26888c5ff9c6d56400b5efc466169bbdb
Mark Shavlovskij <36772847+xidealo@users.noreply.github.com>
2025-02-17 23:23:53 +0300

    Release/1.0.1 (#11)
    * try add new field for user
    
    * try add new field for user

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/entity/UserEntity.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/entity/UserEntity.kt
@@ -1,5 +1,6 @@
 package com.bunbeauty.fooddelivery.data.entity
 
+import com.bunbeauty.fooddelivery.data.entity.cafe.CafeEntity
 import com.bunbeauty.fooddelivery.data.enums.UserRole
 import com.bunbeauty.fooddelivery.data.table.UserTable
 import org.jetbrains.exposed.dao.UUIDEntity
@@ -19,6 +20,7 @@ class UserEntity(uuid: EntityID<UUID>) : UUIDEntity(uuid) {
     var role: UserRole by UserTable.role
 
     var city: CityEntity by CityEntity referencedOn UserTable.city
+    //var cafe: CafeEntity by CafeEntity referencedOn UserTable.cafe
 
     companion object : UUIDEntityClass<UserEntity>(UserTable)
 }
