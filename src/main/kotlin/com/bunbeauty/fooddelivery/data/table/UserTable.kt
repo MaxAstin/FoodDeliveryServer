1811a7fba08fb36802b58b7064dd331c61d83966
Mark Shavlovskij <36772847+xidealo@users.noreply.github.com>
2025-02-17 23:21:25 +0300

    Revert "try add new field for user (#9)"
    This reverts commit 0fd3693442a8c38c07cce6105bdde3fff1296617.

--- src/main/kotlin/com/bunbeauty/fooddelivery/data/table/UserTable.kt
+++ src/main/kotlin/com/bunbeauty/fooddelivery/data/table/UserTable.kt
@@ -1,12 +1,8 @@
 package com.bunbeauty.fooddelivery.data.table
 
 import com.bunbeauty.fooddelivery.data.enums.UserRole
-import com.bunbeauty.fooddelivery.data.table.cafe.CafeTable
 import org.jetbrains.exposed.dao.id.UUIDTable
 
-/**
- * Table for admin app
- * */
 object UserTable : UUIDTable() {
 
     val username = varchar("username", 512).uniqueIndex()
@@ -16,9 +12,5 @@ object UserTable : UUIDTable() {
     val updateNotificationTokenDateTime = varchar("updateNotificationTokenDateTime", 512).nullable()
     val unlimitedNotification = bool("unlimitedNotification").default(true)
     val role = enumeration("role", UserRole::class)
-
-    @Deprecated("Use cafe to connect tables")
     val city = reference("city", CityTable)
-
-    val cafe = reference("cafe", CafeTable)
 }
